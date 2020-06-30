e049fdd4825387cffc23ce81a2ba72b9e1855235
matheus.lima <matheus.lima@lincros.com>
2020-06-30 20:17:10 -0300

    Aulas alura

---
+++ Maven/calculadora/src/main/Calculadora.java
@@ -0,0 +1,7 @@
+package src.main;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        System.out.println("5 mais 5 é" + (5+5));
+    }
+}
---
+++ Maven/calculadora/src/main/test/CalculadoraTest.java
@@ -0,0 +1,5 @@
+package src.main.test;
+
+public class CalculadoraTest {
+
+}
